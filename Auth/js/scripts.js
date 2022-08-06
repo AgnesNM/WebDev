9ee6c71b2fac8c8320c9a99f50c40fa0326fc95c
AgnesNM <agnesndutamwaniki@gmail.com>
2022-08-06 18:25:56 +0300

    question 2 folder code version 1

---
+++ Auth/js/scripts.js
@@ -0,0 +1,15 @@
+function displayFunction() {   
+
+    let username = 'kamau';
+    let email = 'kamaunjoroge@gmail.com';
+    let password = '12345678';
+
+    document.getElementById('submit').value;
+
+    if(username === 'kamau' && email === 'kamaunjoroge@gmail.com' && password === '12345678')  {
+        window.location.assign('todo.html');
+     } else {
+        alert("input values do not match");
+     }
+    
+}
